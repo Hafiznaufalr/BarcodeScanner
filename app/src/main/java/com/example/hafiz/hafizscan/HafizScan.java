28be0b2c6396654ae443b156334fa10d9ceb68ae
Hafiznaufalr <hafiz.nr5758@gmail.com>
2018-08-14 13:37:22 +0700

    first commit

---
+++ app/src/main/java/com/example/hafiz/hafizscan/HafizScan.java
@@ -0,0 +1,17 @@
+package com.example.hafiz.hafizscan;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+
+public class HafizScan extends AppCompatActivity  {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hafiz_scan);
+
+    }
+}
+
